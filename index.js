7165b1ceab475d27df51b310da2b36936968ace5
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-09-08 16:22:50 +0530

    first working version

--- index.js
+++ index.js
@@ -11,10 +11,12 @@ module.exports  = () => {
     try {
       // Open Router config page
       await driver.get('http://10.1.1.1');
+
       // Clear existing username
       await driver.findElement(By.name('username')).clear();
+      // Fill in username
       await driver.findElement(By.name('username')).sendKeys('Abhi');
-      // Login
+      // Fill in password & Login
       await driver.findElement(By.name('password')).sendKeys('abhijith', Key.RETURN);
       
       // Switch to frame that has navbar
@@ -28,12 +30,28 @@ module.exports  = () => {
 
       // Wait until URL Block Link is loaded
       await driver.wait(until.elementLocated(By.linkText('URL Block')), 1000);
-
+      
       // Click on URL Block Link
       await driver.findElement(By.linkText('URL Block')).click();
       
+      // Wait until Form is loaded
+      await driver.wait(until.elementLocated(By.name('urlFQDN')), 1000);
+      
+      // Implicitly wait for the input to be interactable
+      await driver.sleep(1000);
+      
+      // fill in the form (sample site)
+      await driver.findElement(By.xpath('//input[@name="urlFQDN"]')).sendKeys('www.rankyou.com');
+      
       // console.log(chalk.blue('Hello world!'));
 
+      // Click on button
+      await driver.findElement(By.name('addFQDN')).click();
+
+      // Wait until Form is loaded
+      await driver.wait(until.elementLocated(By.name('urlFQDN')), 1000);
+      
+      
     } finally {
       await driver.quit();
     }
