f4d701ac0def49d2ebd617f3871aa6f191d7fd69
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-09-08 15:30:31 +0530

    perform tab switching and open up URL block tab

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { Builder, By, Key, until } = require('selenium-webdriver');
+const chalk = require('chalk');
 
 module.exports  = () => {
   (async function example() {
@@ -8,9 +9,31 @@ module.exports  = () => {
       .build();
 
     try {
-      await driver.get('http://www.google.com/ncr');
-      await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
-      await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+      // Open Router config page
+      await driver.get('http://10.1.1.1');
+      // Clear existing username
+      await driver.findElement(By.name('username')).clear();
+      await driver.findElement(By.name('username')).sendKeys('Abhi');
+      // Login
+      await driver.findElement(By.name('password')).sendKeys('abhijith', Key.RETURN);
+      
+      // Switch to frame that has navbar
+      await driver.switchTo().frame(1);
+
+      // Wait until Advanced Tab Link is loaded
+      await driver.wait(until.elementLocated(By.linkText('Advanced')), 1000);
+
+      // Click on Advanced Link
+      await driver.findElement(By.linkText('Advanced')).click();
+
+      // Wait until URL Block Link is loaded
+      await driver.wait(until.elementLocated(By.linkText('URL Block')), 1000);
+
+      // Click on URL Block Link
+      await driver.findElement(By.linkText('URL Block')).click();
+      
+      // console.log(chalk.blue('Hello world!'));
+
     } finally {
       await driver.quit();
     }
