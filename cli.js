17381f5157080486e19428d143471c440b8b634f
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-09-08 16:56:17 +0530

    use  validator & get data from user input

--- cli.js
+++ cli.js
@@ -9,16 +9,24 @@ const cli = meow(
 	  $ selenium-router-cli [input]
 
 	Options
+		-u, --username TEXT    Router Dashboard username
+		-p, --password TEXT    Router Dashboard password
+		-g, --gateway TEXT	 	 Router Gateway IP
 		-v, --version          Show the version and exit with code 0
 
 	Examples
-	  $ selenium-router-cli -v
+		$ selenium-router-cli --username "ROUTER_USERNAME" --password "ROUTER_PASSWORD"
+		$ selenium-router-cli -u "ROUTER_USERNAME" -p "ROUTER_PASSWORD" -g "10.1.1.2"
 `,
 	{
 		flags: {
 			boolean: ['version'],
+			string: ['username', 'password', 'gateway'],
 			alias: {
+				u: 'username',
+				p: 'password',
 				v: 'version',
+				g: 'gateway',
 			},
 		},
 	}
--- index.js
+++ index.js
@@ -1,21 +1,76 @@
 const { Builder, By, Key, until } = require('selenium-webdriver');
 const chalk = require('chalk');
 
-module.exports = (_options) => {
-	(async function runDriver() {
+const pkg = require('./package.json');
 
+const options = {};
+
+const validate = _options => {
+	if (
+		Object.prototype.hasOwnProperty.call(_options, 'username') ||
+		Object.prototype.hasOwnProperty.call(_options, 'u')
+	) {
+		options.username = _options.username || _options.u;
+	}
+	if (
+		Object.prototype.hasOwnProperty.call(_options, 'password') ||
+		Object.prototype.hasOwnProperty.call(_options, 'p')
+	) {
+		options.password = _options.password || _options.p;
+	}
+	if (
+		Object.prototype.hasOwnProperty.call(_options, 'gateway') ||
+		Object.prototype.hasOwnProperty.call(_options, 'g')
+	) {
+		options.gateway = _options.gateway || _options.g;
+	}
+	if (
+		Object.prototype.hasOwnProperty.call(_options, 'version') ||
+		Object.prototype.hasOwnProperty.call(_options, 'v')
+	) {
+		options.version = _options.version || _options.v;
+	}
+	return null;
+};
+
+const flashError = message => {
+	console.error(chalk.bold.red(`✖ ${message}`));
+	process.exit(1);
+};
+
+module.exports = _options => {
+	validate(_options);
+
+	const { username, password, version, gateway = '10.1.1.1' } = options;
+
+	if (version) {
+		console.log(chalk.bold.green(pkg.version));
+		return;
+	}
+
+	if (!username) {
+		flashError('Error! username is a required field.');
+		return;
+	}
+
+	if (!password) {
+		flashError('Error! password is a required field.');
+		return;
+	}
+
+	(async function runDriver() {
 		const driver = new Builder().forBrowser('chrome').build();
 
 		try {
 			// Open Router config page
-			await driver.get('http://10.1.1.1');
+			await driver.get(`http://${gateway}`);
 
 			// Clear existing username
 			await driver.findElement(By.name('username')).clear();
 			// Fill in username
-			await driver.findElement(By.name('username')).sendKeys('Abhi');
+			await driver.findElement(By.name('username')).sendKeys(username);
 			// Fill in password & Login
-			await driver.findElement(By.name('password')).sendKeys('abhijith', Key.RETURN);
+			await driver.findElement(By.name('password')).sendKeys(password, Key.RETURN);
 
 			// Switch to frame that has navbar
 			await driver.switchTo().frame(1);
@@ -41,14 +96,13 @@ module.exports = (_options) => {
 			// fill in the form (sample site)
 			await driver.findElement(By.xpath('//input[@name="urlFQDN"]')).sendKeys('www.rankyou.com');
 
-      
 			// Click on button
 			await driver.findElement(By.name('addFQDN')).click();
-      
+
 			// Wait until Form is loaded
 			await driver.wait(until.elementLocated(By.name('urlFQDN')), 1000);
-      
-      console.log(chalk.blue('Complete'));
+
+			console.log(chalk.blue('Complete'));
 		} finally {
 			await driver.quit();
 		}
