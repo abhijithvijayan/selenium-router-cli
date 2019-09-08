0ac8bc47e66c4945987de6feff455770b808c805
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-09-08 09:08:20 +0530

    run an async function to open up sample page

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+const { Builder, By, Key, until } = require('selenium-webdriver');
+
 module.exports  = () => {
-  
+  (async function example() {
+
+    let driver = new Builder()
+      .forBrowser('chrome')
+      .build();
+
+    try {
+      await driver.get('http://www.google.com/ncr');
+      await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+      await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+    } finally {
+      await driver.quit();
+    }
+  })();
 }
